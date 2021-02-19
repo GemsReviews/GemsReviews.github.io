b266c0caf26b0b2fcc188e29fb4a4822edfc0f6c
Tomasz Kowalczyk <tomek@kownet.info>
2021-02-19 17:27:12 +0100

    simple node

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const { port } = require('./config');
+const cors = require('cors');
 
 app.use(cors());
 
@@ -9,6 +10,10 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('public'));
     app.get('/*', function (req, res) {
       res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    });
+  } else {
+    app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname + '/public/index.html'));
     });
   }
   
